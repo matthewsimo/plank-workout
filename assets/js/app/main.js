a6f9a399f418fb5645b8c235df6597a316ef1ca7
Matthew Simo <matthew.a.simo@gmail.com>
2014-02-14 14:23:36 -0600

    Rework a bunch of stuff so it won't look so ugly on mobile

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -3,12 +3,16 @@ var PW = angular.module('plankWorkout', [
   'workout',
 ])
 
-.run(function($rootScope, $templateCache) {
+.run(function($rootScope, $templateCache, $window, $timeout) {
   $rootScope.page = {};
 
   // For Dev, so templates don't get cached while being worked on..
   $rootScope.$on('$viewContentLoaded', function() {
     $templateCache.removeAll();
+
+    $timeout(function(){
+      $window.scrollTo(0,1);
+    }, 0);
   });
 
   $rootScope.page.title = "Plank Workout | Begin";
